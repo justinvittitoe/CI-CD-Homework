77a322ca2c89b1a030486e1931d25dd0edaaf74e
Justin Vittitoe <justinvittitoe7@gmail.com>
2025-05-19 23:38:42 -0700

    yml files added with workflows

--- server/dist/seeds/cleanDb.js
+++ server/dist/seeds/cleanDb.js
@@ -2,6 +2,11 @@ import models from '../models/index.js';
 import db from '../config/connection.js';
 export default async (modelName, collectionName) => {
     try {
+        const model = models[modelName];
+        console.log(modelName);
+        if (!model) {
+            throw new Error(`Model ${modelName} does not exist`);
+        }
         let modelExists = await models[modelName].db.db.listCollections({
             name: collectionName
         }).toArray();
