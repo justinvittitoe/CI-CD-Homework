0761b39726326dc9033ecea4b69ff92f5c444cb7
Justin Vittitoe <justinvittitoe7@gmail.com>
2025-05-20 00:16:00 -0700

    update the develop branch

--- client/src/components/Quiz.tsx
+++ client/src/components/Quiz.tsx
@@ -2,7 +2,7 @@ import { useState, } from 'react';
 import type { Question } from '../models/Question.js';
 import { getQuestions } from '../services/questionApi.js';
 
-//new comment 1
+//new comment 2
 
 const Quiz = () => {
   const [questions, setQuestions] = useState<Question[]>([]);
